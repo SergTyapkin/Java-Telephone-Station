da2ec9ba5506faa4aeaf090e026b088a3187fa18
Сергей <tyapkin2002@mail.ru>
2022-05-17 17:01:50 +0300

    initial

---
+++ runServer.cmd
@@ -0,0 +1,3 @@
+D:\Programs\JavaJDK_8u311\bin\javac.exe -cp lib/corba-api-5.0.1.jar src/Telephone/StationServer.java src/Telephone/App/*.java
+
+D:\Programs\JavaJDK_8u311\bin\java.exe -cp src Telephone.StationServer -ORBInitialPort 1050 -ORBInitialHost localhost
