22181c0f8f54945053f09c7d5bc78d5857a3bdb1
Сергей <tyapkin2002@mail.ru>
2022-05-17 22:46:59 +0300

    remove absolute path from .cmd files

--- runServer.cmd
+++ runServer.cmd
@@ -1,3 +1,3 @@
-D:\Programs\JavaJDK_8u311\bin\javac.exe -cp lib/corba-api-5.0.1.jar src/Telephone/StationServer.java src/Telephone/App/*.java
+javac -cp lib/corba-api-5.0.1.jar src/Telephone/StationServer.java src/Telephone/App/*.java
 
-D:\Programs\JavaJDK_8u311\bin\java.exe -cp src Telephone.StationServer -ORBInitialPort 1050 -ORBInitialHost localhost
+java -cp src Telephone.StationServer -ORBInitialPort 1050 -ORBInitialHost localhost
