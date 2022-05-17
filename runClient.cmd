5cfc2ffdd14e069a066f63a371c14fe68c611e15
Сергей <tyapkin2002@mail.ru>
2022-05-18 01:30:45 +0300

    Fix run file

--- runClient.cmd
+++ runClient.cmd
@@ -1,3 +1,3 @@
 javac -cp lib/corba-api-5.0.1.jar src/Telephone/Tube.java src/Telephone/Cell/*.java
 
-java -cp src Telephone.Tube -ORBInitialPort 1050 -ORBInitialHost localhost
+java -cp src Telephone.Tube "<client_name>" -ORBInitialPort 1050 -ORBInitialHost localhost
